785698b2c222f410eea63ecc8cdd8ebaf92972cb
Bluegray Ink <168587909+bluegrayink@users.noreply.github.com>
2025-04-19 16:58:59 +0700

    Update apps.js

--- JS/apps.js
+++ JS/apps.js
@@ -1,7 +1,7 @@
 const uidToPageMap = {
-    "klee.html": ["26586B1A", "F625391A", "Jet00Klee"],
-    "zeta.html": ["26356B1A", "D6C7711A", "Jet00Zeta"],
-    "calendar.html": ["5605761A","86F56F1A", "Jet00Cal"]
+    "klee.html": ["26586B1A", "F625391A", "JET00KLEE"],
+    "zeta.html": ["26356B1A", "D6C7711A", "JET00ZETA"],
+    "calendar.html": ["5605761A","86F56F1A", "JET00CAL"]
 };
 
 // Elements
